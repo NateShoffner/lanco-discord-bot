f17ed5b41428f23260329545eff7ed7c33ca43e1
Nate Shoffner <nate.shoffner@gmail.com>
2023-11-14 12:22:10 -0500

    Status slash command

--- app/cogs/tools/tools.py
+++ app/cogs/tools/tools.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
 from sys import version_info as sysv
 
 
@@ -12,8 +13,8 @@ class Tools(commands.Cog):
     async def ping(self, ctx):
         await ctx.send(f'🏓 {round(self.bot.latency * 1000)} ms.')
 
-    @commands.command(name='status')
-    async def status(self, ctx):
+    @app_commands.command(name="status", description="Show bot status")
+    async def status(self, interaction: discord.Interaction):
         embed = discord.Embed(title = 'Status', description='Bot Status', color = 0x00ff00)
         embed.add_field(name = 'Python', value = f'{sysv.major}.{sysv.minor}.{sysv.micro}')
         embed.add_field(name = 'Discord.py', value = f'{discord.__version__}')
@@ -25,7 +26,7 @@ class Tools(commands.Cog):
         embed.add_field(name = 'Invite', value = f'[Invite Link](https://discord.com/api/oauth2/authorize?client_id={self.bot.user.id}&permissions=8&scope=bot)')
         embed.set_footer(text = f'©{self.bot.user.name}#{self.bot.user.discriminator} | {self.bot.user.id}')
 
-        await ctx.send(embed = embed)
+        await interaction.response.send_message(embed = embed, ephemeral = True)
 
 async def setup(bot):
     await bot.add_cog(Tools(bot))
