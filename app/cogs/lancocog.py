ed49bcafb0eb054341de2de30b9f81d713ff0374
Nate Shoffner <nate.shoffner@gmail.com>
2023-11-14 13:54:52 -0500

    Base cog

---
+++ app/cogs/lancocog.py
@@ -0,0 +1,13 @@
+from discord.ext import commands
+
+
+class LancoCog(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"{self.__get_cog_name()} cog loaded")
+
+    def __get_cog_name(self):
+        return self.__class__.__name__
--- app/cogs/tools/tools.py
+++ app/cogs/tools/tools.py
@@ -3,30 +3,37 @@ from discord.ext import commands
 from discord import app_commands
 from sys import version_info as sysv
 
+from cogs.lancocog import LancoCog
 
-class Tools(commands.Cog):
 
+class Tools(LancoCog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.command(name='ping')
+    @commands.command(name="ping")
     async def ping(self, ctx):
-        await ctx.send(f'🏓 {round(self.bot.latency * 1000)} ms.')
+        await ctx.send(f"🏓 {round(self.bot.latency * 1000)} ms.")
 
     @app_commands.command(name="status", description="Show bot status")
     async def status(self, interaction: discord.Interaction):
-        embed = discord.Embed(title = 'Status', description='Bot Status', color = 0x00ff00)
-        embed.add_field(name = 'Python', value = f'{sysv.major}.{sysv.minor}.{sysv.micro}')
-        embed.add_field(name = 'Discord.py', value = f'{discord.__version__}')
-        embed.add_field(name = 'Guilds', value = f'{len(self.bot.guilds)}')
-        embed.add_field(name = 'Users', value = f'{len(self.bot.users)}')
-        embed.add_field(name = 'Commands', value = f'{len(self.bot.commands)}')
-        embed.add_field(name = 'Cogs', value = f'{len(self.bot.cogs)}')
-        embed.add_field(name = 'Latency', value = f'{round(self.bot.latency * 1000)}ms')
-        embed.add_field(name = 'Invite', value = f'[Invite Link](https://discord.com/api/oauth2/authorize?client_id={self.bot.user.id}&permissions=8&scope=bot)')
-        embed.set_footer(text = f'©{self.bot.user.name}#{self.bot.user.discriminator} | {self.bot.user.id}')
+        embed = discord.Embed(title="Status", description="Bot Status", color=0x00FF00)
+        embed.add_field(name="Python", value=f"{sysv.major}.{sysv.minor}.{sysv.micro}")
+        embed.add_field(name="Discord.py", value=f"{discord.__version__}")
+        embed.add_field(name="Guilds", value=f"{len(self.bot.guilds)}")
+        embed.add_field(name="Users", value=f"{len(self.bot.users)}")
+        embed.add_field(name="Commands", value=f"{len(self.bot.commands)}")
+        embed.add_field(name="Cogs", value=f"{len(self.bot.cogs)}")
+        embed.add_field(name="Latency", value=f"{round(self.bot.latency * 1000)}ms")
+        embed.add_field(
+            name="Invite",
+            value=f"[Invite Link](https://discord.com/api/oauth2/authorize?client_id={self.bot.user.id}&permissions=8&scope=bot)",
+        )
+        embed.set_footer(
+            text=f"©{self.bot.user.name}#{self.bot.user.discriminator} | {self.bot.user.id}"
+        )
 
-        await interaction.response.send_message(embed = embed, ephemeral = True)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+
 
 async def setup(bot):
     await bot.add_cog(Tools(bot))
