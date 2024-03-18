cd7c70c42e3856516aa72c9e6bfb2ed0a34a2556
Nate Shoffner <nate.shoffner@gmail.com>
2024-03-17 23:24:44 -0400

    Dev mode w/ Cog hot-reloading

--- app/cogs/lancocog.py
+++ app/cogs/lancocog.py
@@ -1,18 +1,70 @@
+from dataclasses import dataclass
+import inspect
+import os
 from discord.ext import commands
 import logging
 
 
-class LancoCog(commands.Cog):
-    def __init__(self, bot: commands.Bot):
+@dataclass
+class CogDefinition:
+    """Represents file-system information about a cog"""
+
+    path: str
+    """ The path to the cog directory  """
+    name: str
+    """ The file-system name of the cog"""
+    qualified_name: str
+    """ The fully qualified name of the cog (dot-separated path) """
+    entry_point: str
+    """ The entry point script for the cog"""
+
+
+def get_cog_def(name: str, cogs_dir: str) -> CogDefinition:
+    """Get the cog definition for a cog based on the cog name and the cogs directory"""
+    path = os.path.normpath(os.path.join(cogs_dir, name))
+    cogs_dir_name = os.path.basename(cogs_dir)
+    qualified_name = f"{cogs_dir_name}.{name}.{name}"
+    entry_point = os.path.normpath(f"{path}/{name}.py")
+    return CogDefinition(path, name, qualified_name, entry_point)
+
+
+class LancoCog(commands.Cog, name="LancoCog", description="Base class for all cogs"):
+    def __init__(self, bot: commands.Bot, *args, **kwargs):
         self.bot = bot
-        self.logger = logging.getLogger(self.__get_cog_name())
+        self.logger = logging.getLogger(self.get_cog_name())
+        self.cog_def = None
+
+    def set_cog_def(self, cog_def: CogDefinition):
+        """Set the cog definition"""
+        self.cog_def = cog_def
 
     @commands.Cog.listener()
     async def on_ready(self):
-        self.logger.info(f"{self.__get_cog_name()} cog loaded")
+        self.logger.info(f"{self.get_cog_name()} cog loaded")
 
-    def __get_cog_name(self):
+    def get_cog_name(self):
+        """Get the name of the cog"""
+        return self.qualified_name
+
+    def get_class_name(self):
+        """Get the class name of the cog"""
         return self.__class__.__name__
 
     def get_cog_data_directory(self):
-        return f"data/{self.__get_cog_name()}"
+        """Get the data directory for the cog"""
+        return f"data/{self.get_cog_name()}"
+
+    def get_cog_file_path(self, relative: bool = False):
+        """Get the file path for the cog"""
+        path = inspect.getfile(self.__class__)
+        if relative:
+            path = os.path.relpath(path)
+            # remove top-level directory
+            path = os.path.join(*path.split(os.sep)[1:])
+        return path
+
+    def get_dotted_path(self):
+        """Get the dotted path for the cog"""
+        dotted = self.get_cog_file_path(True).replace(os.sep, ".")
+        dotted = dotted.replace(".py", "")
+        return dotted
--- app/main.py
+++ app/main.py
@@ -9,34 +9,116 @@ from discord.ext import commands
 from logging.handlers import TimedRotatingFileHandler
 from dotenv import load_dotenv
 from peewee import *
+from cogs.lancocog import CogDefinition, LancoCog, get_cog_def
 from utils.dist_utils import get_bot_version, get_commit_hash
 from db import database_proxy
 from sys import version_info as sysv
+from watchfiles import Change, awatch
 
 load_dotenv()
 
 logger = logging.getLogger()
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix=".", intents=intents)
 
 database = SqliteDatabase(os.getenv("SQLITE_DB"))
 database_proxy.initialize(database)
 database.connect()
 
-# TODO probably a better way to inject a database into a cog
-bot.database = database
-
-# TODO set these as env during build
-bot.version = get_bot_version()
-bot.commit = get_commit_hash()
-
-bot.start_time = datetime.datetime.now()
-
 if not os.path.exists("./data"):
     os.makedirs("./data")
 
-COGS_DIR = "./app/cogs"
+COGS_DIR = "app/cogs"
+
+
+class LancoBot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.dev_mode = os.getenv("DEV_MODE", False)
+
+        # TODO set these as env during build
+        self.version = get_bot_version()
+        self.commit = get_commit_hash()
+
+        self.start_time = datetime.datetime.now()
+
+        # TODO probably a better way to inject a database into a cog
+        self.database = database
+
+    def set_dev_mode(self, mode: bool):
+        self.dev_mode = mode
+
+    def get_lanco_cog(self, cog_name: str) -> LancoCog:
+        """Returns a LancoCog instance by name."""
+        return self.get_cog(cog_name)
+
+    def get_lanco_cog_by_class_name(self, class_name: str) -> LancoCog:
+        for c in self.get_lanco_cogs():
+            if c.get_class_name().lower() == class_name.lower():
+                return c
+        return None
+
+    def get_lanco_cog_by_dotted_path(self, dotted_path: str) -> LancoCog:
+        for c in self.get_lanco_cogs():
+            if c.get_dotted_path() == dotted_path:
+                return c
+        return None
+
+    def get_lanco_cogs(self, sort_by_name=True) -> list[LancoCog]:
+        """Get all cogs that are instances of LancoCog"""
+        cogs = [c for c in self.cogs.values() if isinstance(c, LancoCog)]
+        if sort_by_name:
+            return sorted(cogs, key=lambda c: c.get_cog_name())
+        return cogs
+
+    def is_cog_loaded(self, qualified_name: str) -> bool:
+        """Check if a cog with the given name is loaded"""
+        for c in self.get_lanco_cogs():
+            if c.get_dotted_path().lower() == qualified_name.lower():
+                return True
+        return False
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        logger.info(f"Bot ready: {self.user.name} - {self.user.id}")
+
+        async for changes in awatch(COGS_DIR):
+            if not self.dev_mode:
+                continue
+
+            reverse_ordered_changes = sorted(changes, reverse=True)
+
+            for change in reverse_ordered_changes:
+                change_type = change[0]
+                change_path = change[1]
+
+                path = os.path.normpath(change_path)
+                tokens = path.split(os.sep)
+                reversed_tokens = list(reversed(tokens))
+
+                cog_dir_index = reversed_tokens.index(COGS_DIR.split("/")[0])
+                cog_dir = ".".join([token for token in tokens[-cog_dir_index:-1]])
+                cog_name = cog_dir.split(".")[-1]
+                loaded_cog = self.get_lanco_cog_by_dotted_path(cog_dir)
+
+                cog_def = None
+                if loaded_cog:
+                    cog_def = get_cog_def(loaded_cog.get_cog_name(), COGS_DIR)
+                else:
+                    cog_def = get_cog_def(cog_name, COGS_DIR)
+
+                if change_type == Change.deleted:
+                    await unload_cog_by_name(self, cog_def.name)
+                elif change_type == Change.added:
+                    await load_cog(self, cog_def)
+                elif change_type == Change.modified and change_type != (
+                    Change.added or Change.deleted
+                ):
+                    await load_cog(self, cog_def)
+
+
+bot = LancoBot(command_prefix=".", intents=intents)
 
 
 def init_logging():
@@ -56,83 +138,6 @@ def init_logging():
     # logging.getLogger('discord').setLevel(logging.WARNING)
 
     logger.setLevel(logging.INFO)
-
-
-@dataclass
-class CogDefinition:
-    path: str
-    name: str
-    qualified_name: str
-    entry_point: str
-
-
-def get_cog_def(name: str) -> CogDefinition:
-    path = os.path.join(COGS_DIR, name)
-    qualified_name = f"cogs.{name}.{name}"
-    entry_point = f"{path}/{name}.py"
-    return CogDefinition(path, name, qualified_name, entry_point)
-
-
-@dataclass
-class CogLoadResult:
-    definition: CogDefinition
-    loaded: bool = False
-    reloaded: bool = False
-    error: Optional[str] = None
-
-
-def get_cog_by_class_name(class_name: str) -> commands.Cog:
-    current_loaded_cog_names = list(bot.cogs)
-    # we have to inspect the names manually because some cogs might use the name decorator to change the name
-    for name in current_loaded_cog_names:
-        c = bot.get_cog(name)
-        true_name = c.__class__.__name__
-        if true_name.lower() == class_name.lower():
-            return c
-
-    return None
-
-
-async def load_cog(bot: commands.Bot, cog_def: CogDefinition) -> CogLoadResult:
-    """Load a single cog."""
-
-    is_already_loaded = get_cog_by_class_name(cog_def.name) is not None
-    result = CogLoadResult(cog_def)
-
-    try:
-        if is_already_loaded:
-            logger.info(f"Reloading {cog_def.name}: {cog_def.path}")
-            await bot.reload_extension(f"cogs.{cog_def.name}.{cog_def.name}")
-            result.reloaded = True
-        else:
-            logger.info(f"Loading {cog_def.name}: {cog_def.path}")
-            await bot.load_extension(f"cogs.{cog_def.name}.{cog_def.name}")
-            result.loaded = True
-    except Exception as e:
-        logger.error(f"Failed to load cog {cog_def.name}: {e}")
-        result.error = str(e)
-
-    return result
-
-
-async def load_cogs(bot: commands.Bot) -> list[CogLoadResult]:
-    """Load all cogs in the `./cogs` directory."""
-    results = []
-    for entry in os.scandir(COGS_DIR):
-        cog_def = get_cog_def(entry.name)
-        if os.path.isfile(cog_def.entry_point):
-            result = CogLoadResult(cog_def)
-            try:
-                result = await load_cog(bot, cog_def)
-            except:
-                pass
-            results.append(result)
-    return results
-
-
-@bot.event
-async def on_ready():
-    logger.info(f"{bot.user} is now running!")
 
 
 @bot.command(name="sync")
@@ -183,6 +188,9 @@ async def status(interaction: discord.Interaction):
     embed.add_field(name="Commands", value=f"{len(bot.commands)}")
     embed.add_field(name="Slash Commands", value=f"{len(bot.tree.get_commands())}")
     embed.add_field(name="Latency", value=f"{round(bot.latency * 1000)}ms")
+    embed.add_field(
+        name="Dev Mode", value=f"{'Enabled' if bot.dev_mode else 'Disabled'}"
+    )
     uptime = datetime.datetime.now() - bot.start_time
     embed.add_field(
         name="Uptime",
@@ -190,16 +198,32 @@ async def status(interaction: discord.Interaction):
     )
 
     cog_names = [cog.get_cog_name() for cog in bot.cogs.values()]
-    embed.add_field(name=f"Cogs ({len(cog_names)})", value=", ".join(cog_names))
+    embed.add_field(
+        name=f"Cogs ({len(cog_names)})", value=", ".join(cog_names), inline=False
+    )
 
     embed.set_footer(text=f"Version: {bot.version} | Commit: {bot.commit[:7]}")
 
     await interaction.response.send_message(embed=embed)
 
 
+@bot.tree.command(name="devmode")
+@commands.is_owner()
+async def dev(interaction: discord.Interaction):
+    bot.set_dev_mode(not bot.dev_mode)
+
+    embed = discord.Embed(
+        title="Dev Mode",
+        description=f"{'Enabled' if bot.dev_mode else 'Disabled'}",
+        color=0x00FF00,
+    )
+
+    await interaction.response.send_message(embed=embed)
+
+
 @bot.tree.command(name="cogs", description="List loaded cogs")
 async def cogs(interaction: discord.Interaction):
-    cogs = bot.cogs.values()
+    cogs = bot.get_lanco_cogs()
     cog_list = ""
     for cog in cogs:
         cog_list += f"{cog.qualified_name} - {cog.description}\n"
@@ -215,7 +239,9 @@ async def cogs(interaction: discord.Interaction):
 @bot.tree.command(name="reload", description="Reload a cog")
 @commands.is_owner()
 async def reload_cog(interaction: discord.Interaction, cog_name: str):
-    cog_def = get_cog_def(cog_name)
+    """Reload a single cog."""
+
+    cog_def = get_cog_def(cog_name, COGS_DIR)
     result = await load_cog(bot, cog_def)
 
     embed = discord.Embed(
@@ -233,6 +259,70 @@ async def reload_cog(interaction: discord.Interaction, cog_name: str):
     await interaction.response.send_message(embed=embed)
 
 
+@dataclass
+class CogLoadResult:
+    definition: CogDefinition
+    loaded: bool = False
+    reloaded: bool = False
+    unloaded: bool = False
+    error: Optional[str] = None
+    cog: Optional[LancoCog] = None
+
+
+async def load_cog(bot: LancoBot, cog_def: CogDefinition) -> CogLoadResult:
+    """Load a single cog."""
+    is_already_loaded = bot.is_cog_loaded(cog_def.qualified_name)
+    result = CogLoadResult(cog_def)
+
+    try:
+        if is_already_loaded:
+            logger.info(f"Reloading {cog_def.name}: {cog_def.path}")
+
+            await bot.reload_extension(cog_def.qualified_name)
+        else:
+            logger.info(f"Loading {cog_def.name}: {cog_def.path}")
+            await bot.load_extension(cog_def.qualified_name)
+        result.loaded = True
+        result.cog = bot.get_lanco_cog_by_dotted_path(cog_def.qualified_name)
+        result.cog.set_cog_def(cog_def)
+
+    except Exception as e:
+        logger.error(f"Failed to load cog {cog_def.name}: {e}")
+        result.error = str(e)
+
+    return result
+
+
+async def load_cogs(bot: LancoBot) -> list[CogLoadResult]:
+    """Load all cogs in the cogs directory."""
+    results = []
+    for entry in os.scandir(COGS_DIR):
+        cog_def = get_cog_def(entry.name, COGS_DIR)
+        if os.path.isfile(cog_def.entry_point):
+            result = await load_cog(bot, cog_def)
+            results.append(result)
+    return results
+
+
+async def unload_cog_by_name(bot: LancoBot, cog_name: str) -> CogLoadResult:
+    """Unload a single cog."""
+    cog = bot.get_lanco_cog(cog_name)
+    cog_def = get_cog_def(cog_name, COGS_DIR)
+
+    result = CogLoadResult(cog_def)
+    cog = bot.get_lanco_cog_by_class_name(cog_name)
+    if cog:
+        try:
+            logger.info(f"Unloading {cog_name}")
+            await bot.unload_extension(f"cogs.{cog_name}.{cog_name}")
+            result.unloaded = True
+        except Exception as e:
+            logger.error(f"Failed to unload cog {cog_name}: {e}")
+            result.error = str(e)
+
+    return result
+
+
 @bot.tree.command(name="reloadall")
 @commands.is_owner()
 async def reload_all(interaction: discord.Interaction):
@@ -245,28 +335,36 @@ async def reload_all(interaction: discord.Interaction):
 
     results = await load_cogs(bot)
 
-    reloaded_cogs = [result.definition.name for result in results if result.reloaded]
-    loaded_cogs = [result.definition.name for result in results if result.loaded]
-    errored_cogs = [result.definition.name for result in results if result.error]
+    reloaded_cogs = [result.cog.get_cog_name() for result in results if result.reloaded]
+    loaded_cogs = [result.cog.get_cog_name() for result in results if result.loaded]
+    errored_cogs = [result.definition.entry_point for result in results if result.error]
 
     reload_value = "None"
     if len(reloaded_cogs) > 0:
-        reload_value = f"```{', '.join(reloaded_cogs)}```"
+        reload_value = "```"
+        reload_value += "\n".join(reloaded_cogs)
+        reload_value += "```"
     embed.add_field(
-        name=f"Reloaded {len(reloaded_cogs)}:", value=reload_value, inline=False
+        name=f"Reloaded ({len(reloaded_cogs)}):", value=reload_value, inline=False
     )
 
     load_value = "None"
     if len(loaded_cogs) > 0:
-        load_value = f"```{', '.join(loaded_cogs)}```"
-    embed.add_field(name=f"Loaded {len(loaded_cogs)}:", value=load_value, inline=False)
+        load_value = "```"
+        load_value += "\n".join(loaded_cogs)
+        load_value += "```"
+    embed.add_field(
+        name=f"Loaded ({len(loaded_cogs)}):", value=load_value, inline=False
+    )
 
     error_value = "None"
     if len(errored_cogs) > 0:
-        error_value = f"```{', '.join(errored_cogs)}```\n\n"
-        error_value += "Run the `/reload cog_name` command for more info"
+        error_value = "```"
+        error_value += "\n".join(errored_cogs)
+        error_value += "```"
+        error_value += "\nRun the `/reload cog_name` command for more info"
     embed.add_field(
-        name=f"Errors {len(errored_cogs)}:", value=error_value, inline=False
+        name=f"Failed ({len(errored_cogs)}):", value=error_value, inline=False
     )
 
     await interaction.response.send_message(embed=embed)
@@ -276,23 +374,17 @@ async def reload_all(interaction: discord.Interaction):
 @commands.is_owner()
 async def unload_cog(interaction: discord.Interaction, cog_name: str):
     """Unload a single cog."""
-
-    cog = get_cog_by_class_name(cog_name)
+    result = await unload_cog_by_name(bot, cog_name)
 
     embed = discord.Embed(
         title=f"Unloading Cog: {cog_name}",
         color=0x00FF00,
     )
 
-    if cog:
-        try:
-            logger.info(f"Unloading {cog_name}")
-            await bot.unload_extension(f"cogs.{cog_name}.{cog_name}")
-            embed.description = f"Unloaded {cog_name}"
-        except Exception as e:
-            embed.description = f"Error unloading {cog_name}: ```{e}```"
-    else:
-        embed.description = f"Cog {cog_name} not found"
+    if result.unloaded:
+        embed.description = f"Unloaded {cog_name}"
+    elif result.error:
+        embed.description = f"Error unloading {cog_name}: ```{result.error}```"
 
     await interaction.response.send_message(embed=embed)
 
@@ -300,7 +392,6 @@ async def unload_cog(interaction: discord.Interaction, cog_name: str):
 async def main():
     init_logging()
     await load_cogs(bot)
-
     await bot.start(os.getenv("DISCORD_TOKEN"))
 
 
