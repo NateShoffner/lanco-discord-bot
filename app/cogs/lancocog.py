dab6dfa55dfe894e50eaeda6978ed7a6e34d6c68
Nate Shoffner <nate.shoffner@gmail.com>
2024-04-05 23:00:31 -0400

    Move attatchment downloading to base cog

--- app/cogs/lancocog.py
+++ app/cogs/lancocog.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass
+from enum import Enum
 import inspect
 import os
+import uuid
+import aiohttp
+from discord import Message
 from discord.ext import commands
 import logging
 
@@ -26,6 +30,12 @@ def get_cog_def(name: str, cogs_dir: str) -> CogDefinition:
     qualified_name = f"{cogs_dir_name}.{name}.{name}"
     entry_point = os.path.normpath(f"{path}/{name}.py")
     return CogDefinition(path, name, qualified_name, entry_point)
+
+
+class Attatchment:
+    def __init__(self, url: str, filename: str):
+        self.url = url
+        self.filename = filename
 
 
 class LancoCog(commands.Cog, name="LancoCog", description="Base class for all cogs"):
@@ -68,3 +78,73 @@ class LancoCog(commands.Cog, name="LancoCog", description="Base class for all co
         dotted = self.get_cog_file_path(True).replace(os.sep, ".")
         dotted = dotted.replace(".py", "")
         return dotted
+
+    async def download_attachments(self, message: Message) -> list[Attatchment]:
+        """Download attachments from a message"""
+        local_files = []
+        urls = []
+        if message.attachments:
+            self.logger.info("Attachments found in message")
+            for a in message.attachments:
+                url = a.url
+                urls.append(url)
+        elif message.embeds:
+            self.logger.info("Embed found in message")
+            for embed in message.embeds:
+                if embed.image:
+                    url = embed.image.proxy_url
+                if embed.video:
+                    proxy_url = embed.video.proxy_url
+
+                    self.logger.info(f"Proxy URL: {proxy_url}")
+
+                    if "tenor.com" in proxy_url:
+                        # tenor seems to ignore the extension but instead uses the URL path to determine the file type
+
+                        # gif
+                        # https://media.tenor.com/jv1uzXK_ELwAAAAC/fullmetal-alchemist.gif
+                        # mp4
+                        # https://media.tenor.com/jv1uzXK_ELwAAAPo/fullmetal-alchemist.gif
+
+                        # mp4
+                        # https://media.tenor.com/M0DJo6-jF7AAAAPo/anime-responsibilities.mp4
+                        # gif
+                        # https://media.tenor.com/M0DJo6-jF7AAAAAC/anime-responsibilities.gif
+
+                        # hacky way to get the original url and get the original GIF
+                        # Ex: https://images-ext-2.discordapp.net/external/PHVkBmSMxJdxhSl2dlVt9_VL4tiHyn0blDb9ZBNWLjQ/https/media.tenor.com/M0DJo6-jF7AAAAPo/anime-responsibilities.mp4
+
+                        # parse the proxy url and replace the extension, subdomain, and path
+                        tenor_url = proxy_url.split("/https/")[1]
+                        url_split = tenor_url.split("/")
+                        path = url_split[1]
+                        if path.endswith("Po"):
+                            path = path[:-2] + "AC"
+                        new_url = f"https://c.tenor.com/{path}/tenor.gif"
+                        url = new_url
+                        urls.append(url)
+
+                        # https://media.tenor.com/jv1uzXK_ELwAAAPo/fullmetal-alchemist.mp4
+                        # https://c.tenor.com/jv1uzXK_ELwAAAAC/tenor.gif
+                        # https://c.tenor.com/jv1uzXK_ELwAAAC/fullmetal-alchemist.gif
+            self.logger.info(f"URL: {url}")
+
+        for url in urls:
+            # download the image
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    if response.status == 200:
+                        data = await response.read()
+                        random_uuid = uuid.uuid4()
+
+                        if not os.path.exists(self.cache_dir):
+                            os.makedirs(self.cache_dir)
+
+                        ext = url.split(".")[-1].split("?")[0]
+                        filename = os.path.join(self.cache_dir, f"{random_uuid}.{ext}")
+                        with open(filename, "wb") as f:
+                            f.write(data)
+
+                        local_files.append(Attatchment(url, filename))
+
+        return local_files
