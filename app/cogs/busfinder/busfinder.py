3ebcca895ea288b91a412905fade6c8ca6548d6f
Nate Shoffner <nate.shoffner@gmail.com>
2024-05-12 12:07:59 -0400

    RRTA Itinerary

--- app/cogs/busfinder/busfinder.py
+++ app/cogs/busfinder/busfinder.py
@@ -1,18 +1,32 @@
+import datetime
+import math
+import os
+
 import aiohttp
 import discord
+import googlemaps
+import pytz
+from cogs.lancocog import LancoCog
 from discord import app_commands
 from discord.ext import commands
-from cogs.lancocog import LancoCog
-from rrta import RRTAClient
+from rrta import ModeShiftClient, RRTAClient
+from rrta.models.modeshift import Coordinate as ModeShiftCoordinate
+from rrta.models.modeshift import Mode
 
 
 class BusFinder(LancoCog):
     rrta_group = app_commands.Group(name="busfinder", description="RRTA Commands")
 
+    est = pytz.timezone("US/Eastern")
+
     def __init__(self, bot: commands.Bot):
         super().__init__(bot)
         self.bot = bot
+        self.gmaps = googlemaps.Client(key=os.getenv("GMAPS_API_KEY"))
         self.mystop_client = RRTAClient(aiohttp.ClientSession())
+        self.modeshift_client = ModeShiftClient(
+            aiohttp.ClientSession(), os.getenv("MODESHIFT_CLIENT_ID")
+        )
 
     @rrta_group.command(name="routes", description="Get all routes")
     async def rrta(self, interaction: discord.Interaction):
@@ -32,6 +46,81 @@ class BusFinder(LancoCog):
         embed = discord.Embed(
             title=f"RRTA Routes",
             description="\n".join([f"{route}" for route in route_list]),
+        )
+        await interaction.response.send_message(embed=embed)
+
+    @rrta_group.command(name="plan", description="Plan a trip")
+    async def plan(
+        self, interaction: discord.Interaction, origin: str, destination: str
+    ):
+        qualifiers = ["lancaster", "pa", "pennsylvania"]
+
+        if not any(qualifier in origin.lower() for qualifier in qualifiers):
+            origin += " Lancaster, PA"
+
+        if not any(qualifier in destination.lower() for qualifier in qualifiers):
+            destination += " Lancaster, PA"
+
+        origin_coords = self.gmaps.geocode(origin)[0]["geometry"]["location"]
+
+        if origin_coords is None:
+            await interaction.response.send_message(
+                f"Could not find a location named {origin}"
+            )
+            return
+
+        destination_coords = self.gmaps.geocode(destination)[0]["geometry"]["location"]
+        if destination_coords is None:
+            await interaction.response.send_message(
+                f"Could not find a location named {destination}"
+            )
+            return
+
+        planResponse = await self.modeshift_client.get_plan(
+            ModeShiftCoordinate(origin_coords["lat"], origin_coords["lng"]),
+            ModeShiftCoordinate(destination_coords["lat"], destination_coords["lng"]),
+        )
+
+        plan = planResponse.plan
+
+        if plan is None:
+            await interaction.response.send_message(
+                f"Could not find a plan from {origin} to {destination}"
+            )
+            return
+
+        itinerary_list = []
+        for it in plan.itineraries:
+            start_time = datetime.datetime.fromtimestamp(it.startTime).astimezone(
+                self.est
+            )
+            end_time = datetime.datetime.fromtimestamp(it.endTime).astimezone(self.est)
+
+            duration = datetime.timedelta(seconds=it.duration)
+
+            itinerary_list.append(
+                f"{start_time:%I:%M %p} - {end_time:%I:%M %p} \t{math.ceil(duration.seconds / 60)} min"
+            )
+
+            leg_builder = ""
+            has_more = False
+            for i, leg in enumerate(it.legs):
+                if leg.mode == Mode.WALK:
+                    leg_builder += f"Walk {int(leg.duration / 60) }"
+
+                if leg.mode == Mode.BUS:
+                    leg_builder += f"Bus {leg.route}"
+
+                has_more = i < len(it.legs) - 1
+
+                if has_more:
+                    leg_builder += " ➡️ "
+
+            itinerary_list.append(leg_builder)
+
+        embed = discord.Embed(
+            title=f"RRTA trip from {origin} to {destination}",
+            description="\n".join([f"{itinerary}" for itinerary in itinerary_list]),
         )
         await interaction.response.send_message(embed=embed)
 
