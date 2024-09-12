ac3a302e603c36a60244b81f46a228cd57772a63
Nate Shoffner <nate.shoffner@gmail.com>
2024-09-11 23:05:53 -0400

    Peecheck

---
+++ app/cogs/peecheck/peecheck.py
@@ -0,0 +1,19 @@
+from cogs.lancocog import LancoCog
+from discord.ext import commands
+
+
+class PeeCheck(LancoCog, name="PeeCheck", description="PeeCheck cog"):
+
+    def __init__(self, bot: commands.Bot):
+        super().__init__(bot)
+
+    @commands.command(name="peecheck", description="Mark your pee clarity")
+    async def peecheck(self, ctx: commands.Context):
+        msg = await ctx.send("Pee check! How clear is your pee?")
+        colors_squares = ["⬜", "🟨", "🟧", "🟫", "🟥"]
+        for color in colors_squares:
+            await msg.add_reaction(color)
+
+
+async def setup(bot):
+    await bot.add_cog(PeeCheck(bot))
