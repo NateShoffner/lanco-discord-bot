89a6f2177162c1b751409649123cceb9963c99eb
Nate Shoffner <nate.shoffner@gmail.com>
2024-09-25 15:53:47 -0400

    HEIC format support for file fixer

--- app/cogs/filefixer/filefixer.py
+++ app/cogs/filefixer/filefixer.py
@@ -2,16 +2,18 @@ import os
 
 import discord
 import imageio
+import pillow_heif
 from cogs.lancocog import LancoCog
 from discord.ext import commands
+from PIL import Image
 
 
 class FileFixer(LancoCog, name="FileFixer", description="Attempt to fix files"):
+
     def __init__(self, bot: commands.Bot):
         super().__init__(bot)
         self.cache_dir = os.path.join(self.get_cog_data_directory(), "Cache")
 
-    # check for messages with attachments
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.bot:
@@ -24,11 +26,19 @@ class FileFixer(LancoCog, name="FileFixer", description="Attempt to fix files"):
                     self.logger.info("More than one attachment found")
                     return
 
-                if att.filename.lower().endswith(".avif"):
+                fixers = {
+                    "avif": self.fix_avif,
+                    "heic": self.fix_heic,
+                }
+
+                extension = att.filename.lower().split(".")[-1]
+
+                if extension in fixers:
                     downloaded = await self.download_attachments(
                         message, self.cache_dir
                     )
-                    fix_result = await self.fix_avif(message, downloaded[0].filename)
+                    fixer = fixers[extension]
+                    fix_result = await fixer(downloaded[0].filename)
 
                     if fix_result:
                         await self.send_fixed_embed(message, fix_result)
@@ -45,12 +55,25 @@ class FileFixer(LancoCog, name="FileFixer", description="Attempt to fix files"):
         embed.set_image(url=f"attachment://{filename}")
         await original_message.channel.send(file=file, embed=embed)
 
-    async def fix_avif(self, source_message: discord.Message, local_path: str) -> str:
+    async def fix_avif(self, local_path: str) -> str:
         try:
             avif_image = imageio.imread(local_path)
             output_file = local_path.lower().replace(".avif", ".png")
 
             imageio.imwrite(output_file, avif_image, format="PNG")
+            self.logger.info(f"Conversion successful: {local_path} -> {output_file}")
+            return output_file
+        except Exception as e:
+            self.logger.error(f"Error: {e}")
+
+    async def fix_heic(self, local_path: str) -> str:
+        pillow_heif.register_heif_opener()
+
+        try:
+            heic_image = Image.open(local_path)
+            output_file = local_path.lower().replace(".heic", ".png")
+
+            heic_image.save(output_file, "PNG")
             self.logger.info(f"Conversion successful: {local_path} -> {output_file}")
             return output_file
         except Exception as e:
