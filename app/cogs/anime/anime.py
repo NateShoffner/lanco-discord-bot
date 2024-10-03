aaa2508d83d4f10a086efb93df5062a1f980c58d
Nate Shoffner <nate.shoffner@gmail.com>
2024-10-03 17:30:18 -0400

    Use Jikan v4 w/ async

--- app/cogs/anime/anime.py
+++ app/cogs/anime/anime.py
@@ -1,25 +1,24 @@
 import discord
 from cogs.lancocog import LancoCog
 from discord.ext import commands
-from jikanpy import Jikan
-from numpy import True_
+from jikanpy import AioJikan
 
 
 class Anime(LancoCog, name="Anime", description="Anime commands"):
     def __init__(self, bot: commands.Bot):
         super().__init__(bot)
-        self.jikan = Jikan()
 
     @commands.command(name="anime", description="Get anime information")
     async def anime(self, ctx: commands.Context, *, title: str):
-        anime = self.jikan.search("anime", title)
-        print(anime)
-        if not anime:
-            await ctx.send("No anime found")
-            return
-        anime = anime["data"][0]
-        embed = self.build_anime_embed(anime)
-        await ctx.send(embed=embed)
+        async with AioJikan() as aio_jikan:
+            anime = await aio_jikan.search("anime", title)
+            print(anime)
+            if not anime:
+                await ctx.send("No anime found")
+                return
+            anime = anime["data"][0]
+            embed = self.build_anime_embed(anime)
+            await ctx.send(embed=embed)
 
     def build_anime_embed(self, anime):
         embed = discord.Embed(title=anime["title"])
@@ -27,8 +26,8 @@ class Anime(LancoCog, name="Anime", description="Anime commands"):
         embed.set_thumbnail(url=anime["images"]["jpg"]["image_url"])
         embed.add_field(name="Type", value=anime["type"], inline=True)
         embed.add_field(name="Episodes", value=anime["episodes"], inline=True)
-        embed.add_field(name="Score", value=anime["score"], inline=True)
-        embed.add_field(name="URL", value=anime["url"], inline=True)
+        embed.add_field(name="Score", value=anime["score"])
+        embed.add_field(name="URL", value=anime["url"])
         return embed
 
 
