5de2efaee01546c51f1c05471c1fb84c72ece2c7
Nate Shoffner <nate.shoffner@gmail.com>
2024-10-03 17:31:03 -0400

    Correct potential confusion with method arg

--- app/utils/voice_message.py
+++ app/utils/voice_message.py
@@ -7,6 +7,9 @@ import discord
 def is_voice_message(message: discord.Message) -> bool:
     """Check if a message is a voice message"""
 
+    if message.author.bot:
+        return False
+
     if len(message.attachments) != 1:
         return False
 
@@ -16,7 +19,7 @@ def is_voice_message(message: discord.Message) -> bool:
     return True
 
 
-async def download_voice_message(message: discord.Message, filename: str) -> str:
+async def download_voice_message(message: discord.Message, dir: str) -> str:
     """Download a voice message attachment"""
 
     if not is_voice_message(message):
@@ -25,7 +28,7 @@ async def download_voice_message(message: discord.Message, filename: str) -> str
     voice_file = await message.attachments[0].read()
     voice_file = io.BytesIO(voice_file)
 
-    ogg_file_path = os.path.join(filename, f"{message.id}.ogg")
+    ogg_file_path = os.path.join(dir, f"{message.id}.ogg")
     with open(ogg_file_path, "wb") as f:
         f.write(voice_file.getvalue())
 
