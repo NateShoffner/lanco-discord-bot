5d0e3c0d51f6e5723324480cfefc24105638fdb6
Nate Shoffner <nate.shoffner@gmail.com>
2024-11-04 23:50:48 -0500

    Fix broken owner-only commands when teams are utiltized

--- app/utils/command_utils.py
+++ app/utils/command_utils.py
@@ -1,16 +1,22 @@
 import discord
 
 
+def is_bot_owner_or_team_member(interaction: discord.Interaction):
+    if interaction.client.application.team:
+        return interaction.user in interaction.client.application.team.members
+    return interaction.user.id == interaction.client.application.owner.id
+
+
 def is_bot_owner():
     def predicate(interaction: discord.Interaction):
-        return interaction.user.id == interaction.client.application.owner.id
+        return is_bot_owner_or_team_member(interaction)
 
     return discord.app_commands.check(predicate)
 
 
 def is_bot_owner_or_admin():
     def predicate(interaction: discord.Interaction):
-        if interaction.user.id == interaction.client.application.owner.id:
+        if is_bot_owner_or_team_member(interaction):
             return True
         if interaction.guild.owner_id == interaction.user.id:
             return True
