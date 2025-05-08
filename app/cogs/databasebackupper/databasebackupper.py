752290e16df702f8c6061cc20d389b703eae477a
Nate Shoffner <nate.shoffner@gmail.com>
2025-05-07 23:06:59 -0400

    Database backup cog

---
+++ app/cogs/databasebackupper/databasebackupper.py
@@ -0,0 +1,77 @@
+"""
+DatabaseBackupper Cog
+
+Description:
+Routinely backs up the database to a specified directory with a timestamped filename.
+"""
+
+import datetime
+import os
+import shutil
+
+from cogs.lancocog import LancoCog
+from discord.ext import commands, tasks
+from utils.command_utils import is_bot_owner
+
+
+class DatabaseBackupper(
+    LancoCog,
+    name="Database Backupper",
+    description="Routinely backs up the database to a specified directory with a timestamped filename.",
+):
+    def __init__(self, bot):
+        super().__init__(bot)
+
+        self.backup_dir = os.getenv("DATABASE_BACKUP_DIRECTORY")
+        self.backup_filename = os.getenv("DATABASE_BACKUP_FILENAME")
+        self.backup_interval = os.getenv("DATABASE_BACKUP_INTERVAL")
+
+        self.database_path = os.getenv("SQLITE_DB")
+
+        if not self.backup_dir or not self.backup_dir.strip():
+            print("Database backup is disabled. No backup directory specified.")
+            return
+
+        if not os.path.exists(self.backup_dir):
+            os.makedirs(self.backup_dir)
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.logger.info(
+            f"Starting database backup task every {self.backup_interval} seconds"
+        )
+        self.backup_db_task.change_interval(seconds=self.backup_interval)
+        self.backup_db_task.start()
+
+        # perform an initial backup immediately
+        await self.backup_db()
+
+    @tasks.loop()
+    async def backup_db_task(self):
+        await self.backup_db()
+
+    async def backup_db(self):
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        backup_filename = os.path.join(
+            self.backup_dir, self.backup_filename.format(timestamp)
+        )
+
+        self.logger.info(
+            f"Backing up database from {self.database_path} to {backup_filename}"
+        )
+
+        try:
+            shutil.copy2(self.database_path, backup_filename)
+            print(f"Database backed up to {backup_filename}")
+        except Exception as e:
+            print(f"Failed to back up database: {e}")
+
+    @commands.command(name="backup_db", description="Manually back up the database")
+    @is_bot_owner()
+    async def backup_db_command(self, ctx: commands.Context):
+        await self.backup_db()
+        await ctx.send("Database backup triggered.")
+
+
+async def setup(bot):
+    await bot.add_cog(DatabaseBackupper(bot))
