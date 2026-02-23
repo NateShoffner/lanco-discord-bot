bcbb4dce3083cebc44175cabbb66f2fa9e708efa
Nate Shoffner <nate.shoffner@gmail.com>
2026-02-23 11:19:39 -0500

    Admin cog

--- app/cogs/admin/admin.py
+++ app/cogs/admin/admin.py
@@ -1,9 +1,13 @@
+import discord
 from cogs.lancocog import LancoCog
+from discord import app_commands
 from discord.ext import commands
 from utils.command_utils import is_bot_owner_or_admin
 
 
 class Admin(LancoCog, name="Admin", description="Administrative commands"):
+    g = app_commands.Group(name="admin", description="Administrative commands")
+
     def __init__(self, bot: commands.Bot):
         super().__init__(bot)
 
@@ -16,6 +20,27 @@ class Admin(LancoCog, name="Admin", description="Administrative commands"):
         await message.delete()
         # delete the command message
         await ctx.message.delete()
+
+    @g.command(name="delete", description="Delete a message by ID")
+    @is_bot_owner_or_admin()
+    async def delete_by_id(self, interaction: discord.Interaction, message_id: int):
+        """Delete a message by ID"""
+        channel = interaction.channel
+        try:
+            message = await channel.fetch_message(message_id)
+            await message.delete()
+            await interaction.response.send_message(
+                f"Message {message_id} deleted.", ephemeral=True
+            )
+        except discord.NotFound:
+            await interaction.response.send_message(
+                f"Message {message_id} not found.", ephemeral=True
+            )
+        except discord.Forbidden:
+            await interaction.response.send_message(
+                "I do not have permission to delete messages in this channel.",
+                ephemeral=True,
+            )
 
 
 async def setup(bot):
