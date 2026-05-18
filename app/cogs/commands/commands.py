83914e524b09d8a95479dd262ffb857a610613b7
Nate Shoffner <nate.shoffner@gmail.com>
2026-05-18 19:30:37 -0400

    Add support for AI commands/modal-based setup

--- app/cogs/commands/commands.py
+++ app/cogs/commands/commands.py
@@ -1,12 +1,161 @@
 import discord
 from cogs.lancocog import LancoCog
-from discord import app_commands
+from discord import app_commands, ui
 from discord.ext import commands
+from pydantic import BaseModel, Field
+from pydantic_ai import Agent, BinaryContent
 from reactionmenu import ReactionButton, ReactionMenu
 from utils.command_utils import is_bot_owner_or_admin
 from utils.tracked_message import track_message_ids
 
 from .models import CustomCommands
+
+
+class AICommandResponse(BaseModel):
+    response: str = Field(
+        ...,
+        description="The AI-generated response for the command.",
+    )
+
+
+class CommandTypes:
+    BASIC = "basic"
+    AI = "ai"
+
+
+class CommandModal(ui.Modal, title="Command Info"):
+    command_name = ui.TextInput(
+        label="Command Name:",
+        style=discord.TextStyle.short,
+        required=True,
+    )
+
+    type_selection = ui.Label(
+        text="Select Command Type:",
+        description=" Select the type of command.",
+        component=ui.Select(
+            placeholder="Choose command type",
+            options=[
+                discord.SelectOption(
+                    label="Basic",
+                    description="A basic custom command.",
+                    value=CommandTypes.BASIC,
+                    default=True,
+                ),
+                discord.SelectOption(
+                    label="AI",
+                    description="An AI-powered command.",
+                    value=CommandTypes.AI,
+                ),
+            ],
+            min_values=1,
+            max_values=1,
+            required=False,
+        ),
+    )
+
+    channel_selection = ui.Label(
+        text="Select Channel (Optional):",
+        description=" Select a channel where this command can be used.",
+        component=ui.ChannelSelect(
+            channel_types=[discord.ChannelType.text], required=False
+        ),
+    )
+
+    cooldown_selection = ui.Label(
+        text="Set Cooldown (Optional):",
+        description=" Set a cooldown for this command.",
+        component=ui.Select(
+            placeholder="Choose cooldown",
+            options=[
+                discord.SelectOption(label="10 seconds", value="10"),
+                discord.SelectOption(label="30 seconds", value="30"),
+                discord.SelectOption(label="1 minute", value="60"),
+                discord.SelectOption(label="5 minutes", value="300"),
+                discord.SelectOption(label="10 minutes", value="600"),
+                discord.SelectOption(label="30 minutes", value="1800"),
+                discord.SelectOption(label="1 hour", value="3600"),
+            ],
+            min_values=1,
+            max_values=1,
+            required=False,
+        ),
+    )
+
+    command_response = ui.TextInput(
+        label="Command Response/Prompt:",
+        style=discord.TextStyle.long,
+        required=True,
+    )
+
+    def __init__(self, command: CustomCommands = None):
+        super().__init__(timeout=None)
+        self.command = command
+        if command:
+            self.command_name.default = command.command_name
+            self.command_response.default = command.command_response
+
+            for option in self.type_selection.component.options:
+                option.default = option.value == command.command_type
+
+            if command.channel_id:
+                self.channel_selection.component.default_values = [
+                    discord.Object(id=command.channel_id)
+                ]
+
+            if command.cooldown:
+                for option in self.cooldown_selection.component.options:
+                    option.default = option.value == str(command.cooldown)
+
+    async def on_submit(self, interaction: discord.Interaction) -> None:
+        edit = self.command is not None
+
+        def format_message_response(message: str) -> str:
+            """Format the message to allow for newlines and other formatting"""
+            response = message.replace("\\n", "\n")
+            return response
+
+        config, created = CustomCommands.get_or_create(
+            guild_id=interaction.guild_id,
+            command_name=self.command_name.value.lower(),
+        )
+
+        config.command_response = format_message_response(self.command_response.value)
+        config.command_type = self.type_selection.component.values[0]
+
+        if self.channel_selection.component.values:
+            config.channel_id = self.channel_selection.component.values[0].id
+        else:
+            config.channel_id = None
+
+        if self.cooldown_selection.component.values:
+            config.cooldown = int(self.cooldown_selection.component.values[0])
+        else:
+            config.cooldown = 0
+
+        config.last_updated = discord.utils.utcnow()
+        config.author = interaction.user.id
+        config.save()
+
+        embed_title = "Command Edited" if edit else "Command Created"
+        embed = discord.Embed(title=embed_title, color=discord.Color.blue())
+        embed.add_field(name="Command Name", value=self.command_name.value)
+        embed.add_field(
+            name="Command Response/Prompt", value=self.command_response.value
+        )
+        embed.add_field(
+            name="Command Type", value=self.type_selection.component.values[0]
+        )
+
+        if self.channel_selection.component.values:
+            channel_mention = self.channel_selection.component.values[0].mention
+            embed.add_field(name="Channel", value=channel_mention)
+
+        if self.cooldown_selection.component.values:
+            cooldown_value = self.cooldown_selection.component.values[0]
+            embed.add_field(name="Cooldown (seconds)", value=cooldown_value)
+
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
 
 class Commands(LancoCog, name="Commands", description="Custom guild commands"):
@@ -18,52 +167,17 @@ class Commands(LancoCog, name="Commands", description="Custom guild commands"):
     def __init__(self, bot: commands.Bot):
         super().__init__(bot)
         self.bot.database.create_tables([CustomCommands])
+        self.agent = Agent(
+            model="openai:gpt-4o-mini",
+            system_prompt="Generate a concise and relevant response based on the user's command prompt.",
+            output_type=AICommandResponse,
+        )
 
     @commands_group.command(name="create", description="Create a custom command")
     @is_bot_owner_or_admin()
-    async def create(
-        self,
-        interaction: discord.Interaction,
-        command_name: str,
-        command_response: str,
-        channel: discord.TextChannel = None,
-    ):
-        if CustomCommands.get_or_none(
-            guild_id=interaction.guild_id, command_name=command_name.lower()
-        ):
-            embed = discord.Embed(
-                title="Command already exists",
-                description=f"Command {command_name} already exists",
-                color=discord.Color.red(),
-            )
-            await interaction.response.send_message(embed=embed, ephemeral=True)
-
-        command_response = self.format_message_response(command_response)
-
-        command = CustomCommands.create(
-            guild_id=interaction.guild_id,
-            command_name=command_name.lower(),
-            command_response=command_response,
-            channel_id=channel.id if channel else None,
-        )
-
-        embed = discord.Embed(
-            title="Command created",
-            description=f"Command {command_name} created",
-            color=discord.Color.green(),
-        )
-        embed.add_field(
-            name="Command response",
-            value=command_response,
-            inline=False,
-        )
-        if channel:
-            embed.add_field(
-                name="Channel",
-                value=channel.mention,
-                inline=False,
-            )
-        await interaction.response.send_message(embed=embed)
+    async def create2(self, interaction: discord.Interaction):
+        modal = CommandModal()
+        await interaction.response.send_modal(modal)
 
     @commands_group.command(name="delete", description="Delete a custom command")
     @is_bot_owner_or_admin()
@@ -98,9 +212,7 @@ class Commands(LancoCog, name="Commands", description="Custom guild commands"):
 
     @commands_group.command(name="edit", description="Edit a custom command")
     @is_bot_owner_or_admin()
-    async def edit(
-        self, interaction: discord.Interaction, command_name: str, command_response: str
-    ):
+    async def edit(self, interaction: discord.Interaction, command_name: str):
         command = CustomCommands.get_or_none(
             guild_id=interaction.guild_id, command_name=command_name.lower()
         )
@@ -114,21 +226,8 @@ class Commands(LancoCog, name="Commands", description="Custom guild commands"):
             await interaction.response.send_message(embed=embed, ephemeral=True)
             return
 
-        command_response = self.format_message_response(command_response)
-        command.command_response = command_response
-        command.save()
-
-        embed = discord.Embed(
-            title="Command edited",
-            description=f"Command {command_name} edited",
-            color=discord.Color.green(),
-        )
-        embed.add_field(
-            name="Command response",
-            value=command_response,
-            inline=False,
-        )
-        await interaction.response.send_message(embed=embed)
+        modal = CommandModal(command=command)
+        await interaction.response.send_modal(modal)
 
     @commands_group.command(name="list", description="List all custom commands")
     async def list(self, interaction: discord.Interaction):
@@ -212,12 +311,37 @@ class Commands(LancoCog, name="Commands", description="Custom guild commands"):
                 ):
                     return
 
-                return await message.channel.send(command.command_response)
+                # TODO handle cooldowns
+                # TODO handle exclusive owner
 
-    def format_message_response(self, message: str) -> str:
-        """Format the message to allow for newlines and other formatting"""
-        response = message.replace("\\n", "\n")
-        return response
+                self.logger.info(
+                    f"Executing custom command '{command.command_name}' in guild '{message.guild.name}' ({message.guild.id}) by user '{message.author}' ({message.author.id})"
+                )
+
+                msg = None
+
+                if command.command_type == CommandTypes.AI:
+                    await message.channel.typing()
+                    response = await self.agent.run(command.command_response)
+
+                    # limit it for a discord message
+                    if len(response.output.response) > 2000:
+                        response.output.response = (
+                            response.output.response[:1997] + "..."
+                        )
+                        self.logger.info(
+                            "Message was too long, truncated to 2000 characters."
+                        )
+
+                    msg = await message.channel.send(response.output.response)
+
+                else:
+                    msg = await message.channel.send(command.command_response)
+
+                command.last_used = discord.utils.utcnow()
+                command.save()
+
+                return msg
 
 
 async def setup(bot):
--- app/cogs/commands/models.py
+++ app/cogs/commands/models.py
@@ -5,8 +5,14 @@ from peewee import *
 class CustomCommands(BaseModel):
     guild_id = IntegerField()
     command_name = CharField()
-    command_response = CharField()
+    command_response = CharField(null=True)
     channel_id = IntegerField(null=True)
+    command_type = CharField(default="basic")
+    last_updated = DateTimeField(null=True)
+    author = BigIntegerField(null=True)
+    cooldown = IntegerField(default=0)
+    last_used = DateTimeField(null=True)
+    owner = BigIntegerField(null=True)
 
     class Meta:
         table_name = "custom_commands"
