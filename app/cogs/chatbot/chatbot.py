NateShoffner/lanco-discord-bot

commit 44a9a994f391d8f1a7b5a53f6901afd0df3890e6

fix(chatbot): use tracked message check instead of embed heuristic for replies (#160)

app/cogs/chatbot/chatbot.py
@@ -12,6 +12,7 @@ from PIL import Image
 from pydantic_ai import Agent, BinaryContent, ImageUrl
 from pydantic_ai.messages import ModelMessage
 from utils.ai_utils import run_agent
+from utils.tracked_message import is_message_tracked
 
 _MENTION_RE = re.compile(r"<@!?(\d+)>")
 
@@ -216,7 +217,7 @@ class ChatBot(
                 message.reference.message_id
             )
             if referenced_msg.author.id == self.bot.user.id:
-                if referenced_msg.embeds:
+                if is_message_tracked(referenced_msg.id):
                     return
                 is_reply = True
             elif referenced_msg.author.bot:

commit a5a37978712298f8246e056505a76f10ec44c6d8

fix(chatbot): fix reply regression from #160 embed check removal

app/cogs/chatbot/chatbot.py
@@ -232,7 +232,7 @@ class ChatBot(
                 message.reference.message_id
             )
             if referenced_msg.author.id == self.bot.user.id:
-                if is_message_tracked(referenced_msg.id):
+                if referenced_msg.embeds or is_message_tracked(referenced_msg.id):
                     return
                 is_reply = True
             elif referenced_msg.author.bot: